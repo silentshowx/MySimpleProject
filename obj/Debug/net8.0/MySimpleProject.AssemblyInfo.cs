fcd8f6d886abdd86d85a964041fa75051adcb423
silentshowx <ivota.ivota@gmail.com>
2024-03-07 11:53:55 +0100

    Doc changes branch 2

--- obj/Debug/net8.0/MySimpleProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/MySimpleProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MySimpleProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c85d37e127b41db8e469ee71624cd43bdaaf1ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ed32415c56c23eec3f2db2f40310c5aedaf81bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MySimpleProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MySimpleProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
